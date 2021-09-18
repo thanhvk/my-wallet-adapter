77441a84be050bfccf345f74461b113d325d5230
thanhvk <thanhvk.w3s@gmail.com>
2021-09-18 13:23:04 +0700

    add app header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import styled from 'styled-components';
 import './App.less';
 import Wallet from './Wallet';
 
+const Layout = styled.div`
+  max-width: 1200px;
+  margin: auto;
+`
+
+const Main = styled.main``
+const Header = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px 0;
+`
+
 function App() {
   return (
-    <div className="App">
-      <main>
+    <Layout className="App">
+      <Header>
         <Wallet />
-      </main>
-    </div>
+      </Header>
+      <Main>
+        Main content in here!
+      </Main>
+    </Layout>
   );
 }
 
--- src/Wallet.js
+++ src/Wallet.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { Space } from 'antd';
+import { WalletOutlined } from '@ant-design/icons';
 import {
     WalletModalProvider,
     WalletDisconnectButton,
     WalletMultiButton
 } from '@solana/wallet-adapter-ant-design';
+import { useWallet } from '@solana/wallet-adapter-react';
 
 const Wallet = () => {
+    const { publicKey } = useWallet();
+
     return (
         <WalletModalProvider>
-            <WalletMultiButton />
-            <WalletDisconnectButton />
+            <Space>
+                {!publicKey && <WalletMultiButton />}
+                {publicKey && (
+                    <>
+                        <a target="_blank" rel="noreferrer" href={`https://solscan.io/account/${publicKey.toBase58()}`}>
+                            <WalletOutlined />
+                            {publicKey.toBase58().slice(0, 6) + "..." + publicKey.toBase58().slice(-6)}
+                        </a>
+                        <WalletDisconnectButton />
+                    </>
+                )}
+            </Space>
         </WalletModalProvider>
     );
 };

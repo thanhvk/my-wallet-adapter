b0b649f7f042d7372f38a4003bda9ab8eee50a40
thanhvk <thanhvk.w3s@gmail.com>
2021-09-24 22:06:01 +0700

    add send spl token

--- src/components/Balances/SendToken.js
+++ src/components/Balances/SendToken.js
@@ -1,14 +1,16 @@
-import React, { useMemo, createRef, useCallback, useState } from 'react';
+import React, { useMemo, useCallback, useState } from 'react';
 import { Form, Input, Button, Select, Space, Divider, Typography, message } from 'antd';
-import { SendOutlined, DollarCircleOutlined, CaretRightOutlined } from '@ant-design/icons';
-import { Transaction } from '@solana/web3.js';
+import { DollarCircleOutlined, CaretRightOutlined } from '@ant-design/icons';
+import { Transaction, PublicKey, TransactionInstruction, SYSVAR_RENT_PUBKEY, SystemProgram } from '@solana/web3.js';
 import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import BigNumber from 'bignumber.js';
+import { TokenInstructions } from '@project-serum/serum';
 
 import { useSolanaTokens } from '../../providers/general';
 import { formatNumber, abbr } from '../../utils';
 import { TokenOptionContent, SplPanel, SplPanelHeader, SplPanelContent } from './styles';
+import { ASSOCIATED_TOKEN_PROGRAM_ID } from '../../constants';
 
 const { Option } = Select;
 const { Text } = Typography
@@ -30,27 +32,6 @@ const SendToken = ({ accounts }) =>  {
 
     return acc;
   }, {})
-
-  const formRef = createRef();
-
-  const onTokenChange = (tokenAddress) => {
-    console.log(tokenAddress, 'token change====================');
-  };
-
-  const onFinish = (values) => {
-    console.log(values);
-  };
-
-  // const onReset = () => {
-  //   formRef.current.resetFields();
-  // };
-
-  // const onFill = () => {
-  //   formRef.current.setFieldsValue({
-  //     note: 'Hello world!',
-  //     gender: 'male',
-  //   });
-  // };
 
   const onSendSunnyToken = useCallback(async () => {
     try {
@@ -77,7 +58,135 @@ const SendToken = ({ accounts }) =>  {
     }
   }, [connection, publicKey, sendTransaction])
 
-  const [showSplTokenForm, setShowSplTokenForm] = useState(false)
+  // Send SPL token
+  const [form] = Form.useForm();
+  const [showSplTokenForm, setShowSplTokenForm] = useState(true)
+
+  // const handleSenTokenFormValuesChange = async (values) => {
+  //   try {
+  //     const validateFields = await form.validateFields()
+  //     console.log(validateFields, 'get validateFields ==============================')
+  //   } catch (error) {
+  //     console.log(error && error.errorFields, 'errors ===========================');
+  //   }
+  // }
+
+  const getOrCreateAssociatedTokenAddress = async (
+    desOwnerAddress, // public key
+    tokenMintAddress // public key
+  ) => {
+    const associatedTokenAddress = await PublicKey.findProgramAddress(
+      [
+        desOwnerAddress.toBuffer(),
+        TokenInstructions.TOKEN_PROGRAM_ID.toBuffer(),
+        tokenMintAddress.toBuffer(),
+      ],
+      ASSOCIATED_TOKEN_PROGRAM_ID,
+    );
+
+    const associatedAccountInfo = await connection.getAccountInfo(
+      associatedTokenAddress[0],
+    );
+
+    if (associatedAccountInfo) return associatedTokenAddress[0]
+
+    return await createAssociatedTokenAccountInternal(
+      tokenMintAddress,
+      desOwnerAddress,
+      associatedTokenAddress[0],
+    );
+  }
+
+  function createAssociatedTokenAccountInstruction(
+    mint, // PublicKey,
+    associatedAccount, // PublicKey,
+    owner, // PublicKey,
+    payer, // PublicKey,
+  ) {
+    const data = Buffer.alloc(0);
+
+    let keys = [
+      {pubkey: payer, isSigner: true, isWritable: true},
+      {pubkey: associatedAccount, isSigner: false, isWritable: true},
+      {pubkey: owner, isSigner: false, isWritable: false},
+      {pubkey: mint, isSigner: false, isWritable: false},
+      {pubkey: SystemProgram.programId, isSigner: false, isWritable: false},
+      {pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false},
+      {pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false},
+    ];
+
+    return new TransactionInstruction({
+      keys,
+      programId: ASSOCIATED_TOKEN_PROGRAM_ID,
+      data,
+    });
+  }
+
+  async function createAssociatedTokenAccountInternal(
+    tokenAddress,
+    owner, // PublicKey
+    associatedAddress, // PublicKey
+  ) {
+    const transaction = new Transaction().add(
+      createAssociatedTokenAccountInstruction(
+        tokenAddress, // token public key
+        associatedAddress,
+        owner,
+        publicKey, // payer
+      ),
+    )
+
+    await sendTransaction(
+      transaction,
+      connection
+    );
+
+    return associatedAddress;
+  }
+
+  const sendSplToken = async (
+    fromTokenAddress,
+    desTokenAddress,
+    amount, // include decimals
+  ) => {
+    // Add token transfer instructions to transaction
+    const transaction = new Transaction().add(
+      Token.createTransferInstruction(
+        TOKEN_PROGRAM_ID,
+        fromTokenAddress,
+        desTokenAddress,
+        publicKey,
+        [],
+        amount,
+      ),
+    );
+  
+    return await sendTransaction(transaction, connection);
+  }
+
+  const onSendSplToken = async (values) => {
+    try {
+      const { tokenAddress, amount, destination } = values;
+
+      const desOwnerAddress = new PublicKey(destination);
+      const tokenMintAddress = new PublicKey(tokenAddress); // mint address
+      const amountToken = BigNumber(amount).times(10**tokens[tokenAddress].token.tokenAmount.decimals).toNumber();
+
+      const desTokenAddress = await getOrCreateAssociatedTokenAddress(desOwnerAddress, tokenMintAddress)
+      const fromTokenAddress = tokens[tokenAddress].account.address
+
+      const signature = await sendSplToken(
+        fromTokenAddress, 
+        desTokenAddress.toBase58(), 
+        amountToken
+      )
+      const result = await connection.confirmTransaction(signature, 'processed');
+      message.success(`Success send at slot ${result.context.slot}`);
+    } catch (error) {
+      console.log(error, '==================================');
+      message.error('Error');
+    }
+  }
 
   return (
     <>
@@ -100,14 +209,17 @@ const SendToken = ({ accounts }) =>  {
         {showSplTokenForm && (
           <SplPanelContent>
             <Form 
-              layout="vertical" 
-              ref={formRef} 
+              layout="vertical"
+              form={form}
               size="large"
-              name="sendToken" 
-              onFinish={onFinish}
+              name="sendToken"
+              // validate form and set validte value
+              // onValuesChange={handleSenTokenFormValuesChange}
+              // sen spl token
+              onFinish={onSendSplToken}
             >
               <Form.Item
-                name="token"
+                name="tokenAddress"
                 label="Token"
                 rules={[
                   {
@@ -117,7 +229,6 @@ const SendToken = ({ accounts }) =>  {
               >
                 <Select
                   placeholder="Select a token"
-                  onChange={onTokenChange}
                   allowClear
                 >
                   {
@@ -166,7 +277,7 @@ const SendToken = ({ accounts }) =>  {
 
               <Form.Item>
                 <Button type="primary" htmlType="submit">
-                  <Space>Send <SendOutlined /></Space>
+                  <Space>Send</Space>
                 </Button>
               </Form.Item>
             </Form>
